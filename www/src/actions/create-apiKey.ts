e209fc555b70701788bfdd9cfbcfca6c55b16ad1
Evansso <marsapollo3@gmail.com>
2024-10-21 11:58:37 +0300

    hello again

--- www/src/actions/create-apiKey.ts
+++ www/src/actions/create-apiKey.ts
@@ -5,20 +5,11 @@ import { formSchema } from "@/lib/zod";
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export async function createApiKey(prevState: any, formData: FormData) {
 	try {
-		const data = Object.fromEntries(formData.entries());
 		const customData = formData.get("custom-data");
-		const parsed = formSchema.safeParse(data || customData);
-
-		if (!parsed.success) {
-			return {
-				success: false,
-				error: parsed.error.flatten().fieldErrors,
-			};
-		}
 
 		const postData = await fetch("https://keys.mpesaflow.com/keys/create", {
 			method: "POST",
-			body: JSON.stringify(parsed.data),
+			body: JSON.stringify(customData),
 		}).then((res) => res.json());
 
 		return {
