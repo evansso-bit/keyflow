65029d8de6fe1a2343da0888928a8215cb02c43e
Evansso <marsapollo3@gmail.com>
2024-10-21 16:44:49 +0300

    asdf

--- www/src/actions/verify-apiKey.ts
+++ www/src/actions/verify-apiKey.ts
@@ -3,18 +3,25 @@
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export async function verifyApiKeyAction(prevState: any, formData: FormData) {
 	try {
-		const key = formData.get("key");
+		const key = formData.get("key") as string;
+
+		console.log(key);
 
 		const response = await fetch("https://keys.mpesaflow.com/keys/verify", {
 			method: "POST",
-			body: JSON.stringify({ key }),
+			body: JSON.stringify({
+				key: key,
+			}),
 		}).then((res) => res.json());
+
+		console.log(response);
 
 		return {
 			data: response,
 			message: response.valid === false ? "Invalid API key" : "Valid API key",
 		};
 	} catch (error) {
+		console.log(error);
 		return {
 			valid: false,
 			error: `Error: ${error}`,
