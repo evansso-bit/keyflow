749b2f7db93cf4895b03992a910b511c717ac3ac
Evansso <marsapollo3@gmail.com>
2024-10-21 11:47:08 +0300

    added toaster

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { MainNav } from "@/components/main-nav";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.className} antialiased w-full min-h-screen bg-background text-foreground`}
       >
         <MainNav />
+        <Toaster />
         {children}
       </body>
     </html>
